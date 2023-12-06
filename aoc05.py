6aa5b8dc1025049dec8856274a647d567e228fce
tcatsuko <58714618+tcatsuko@users.noreply.github.com>
2023-12-05 21:39:30 -0600

    Day 5: If You Give A Seed A Fertilizer

--- aoc05.py
+++ aoc05.py
@@ -176,7 +176,69 @@ def check_overlap(range1, range2):
     y1 = range2[0]
     y2 = range2[1]
     return (x2 >= y1) and (x1 <= y2)
+
+lowest_location = float('inf')
+
+# Build up the structure for part 2
+game_ranges = []
+# seed-to-soil
+rule_range = []
+current_rule = []
+for seed in seed_soil:
+    
+    this_range = seed_soil[seed]['range']
+    seed_range = [seed, seed + this_range - 1]
+    soil_start = seed_soil[seed]['soil']
+    current_rule += [(seed_range, soil_start)]
+rule_range += [current_rule]
+current_rule = []
+for soil in soil_fert:
+    this_range = soil_fert[soil]['range']
+    soil_range = [soil, soil + this_range - 1]
+    fert_start = soil_fert[soil]['fert']
+    current_rule += [(soil_range, fert_start)]
+rule_range += [current_rule]
+current_rule = []
+for fert in fert_water:
+    this_range = fert_water[fert]['range']
+    fert_range = [fert, fert + this_range - 1]
+    water_start = fert_water[fert]['water']
+    current_rule += [(fert_range, water_start)]
+rule_range += [current_rule]
+current_rule = []
+for water in water_light:
+    this_range = water_light[water]['range']
+    water_range = [water, water + this_range - 1]
+    light_start = water_light[water]['light']
+    current_rule += [(water_range, light_start)]
+rule_range += [current_rule]
+current_rule = []
+for light in light_temp:
+    this_range = light_temp[light]['range']
+    light_range = [light, light + this_range - 1]
+    temp_start = light_temp[light]['temp']
+    current_rule += [(light_range, temp_start)]
+rule_range += [current_rule]
+current_rule = []
+for temp in temp_humid:
+    this_range = temp_humid[temp]['range']
+    temp_range = [temp, temp + this_range - 1]
+    humid_start = temp_humid[temp]['humid']
+    current_rule += [(temp_range, humid_start)]
+rule_range += [current_rule]
+current_rule = []
+for humid in humid_location:
+    this_range = humid_location[humid]['range']
+    humid_range = [humid, humid + this_range - 1]
+    location_start = humid_location[humid]['location']
+    current_rule += [(humid_range, location_start)]
+rule_range += [current_rule]
+
+new_seed_ranges = seed_ranges[0]
+
 def split_ranges(source_range, rule_range, translate_start):
+    # will always return the source range by default, assumes no overlap
+    translated = False
     x1 = source_range[0]
     x2 = source_range[1]
     y1 = rule_range[0]
@@ -184,14 +246,94 @@ def split_ranges(source_range, rule_range, translate_start):
     # Check if source range is completely within compared range
     # y1~x1---x2~y2
     before_range = []
-    overlapped_range = []
+    overlapped_range = source_range
     after_range = []
     if (x1 >= y1) and (x2 <= y2):
-        overlapped_range = [translate_start + (x1 - y1_, translate_start + (y2 - x2))]
-    
-    
-    return (before_range, overlapped_range, after_range)
-lowest_location = float('inf')
+        overlapped_range = [translate_start + x1 - y1, translate_start + x2 - y1]
+        translated = True
+    # check if rule range is completely within source range
+    # x1~y1---y2~x2
+    elif (y1 >= x1) and (y2 <= x2):
+        if x1 != y1:
+            before_range = [x1, y1 - 1]
+        overlapped_range = [translate_start, translate_start + y2 - y1]
+        translated = True
+        if x2 != y2:
+            after_range = [y2 + 1, x2]
+    # check if rule range overlaps on left side
+    # y1---x1----y2---x2
+    elif (y1 <= x1) and (x2 >= y2) and (y2 >= x1):
+        
+        overlapped_range = [translate_start + (x1-y1), translate_start + y2 - x1]
+        translated = True
+        if x2 != y2:
+            after_range = [y2 + 1, x2]
+    # check if rule range overlaps on right side
+    # x1---y1---x2---y2
+    elif (x1 <= y1) and (x2 < y2) and (x2 >= y1):
+        if x1 != y1:
+            before_range = [x1, y1 - 1]
+        overlapped_range = [translate_start, translate_start + x2 - y1]
+        translated = True
+        
+    return (before_range, overlapped_range, after_range, translated)
+
+
+def translate_points(in_range, next_ranges):
+    global lowest_location
+    if len(next_ranges) == 7:
+        print('Processing seed-to-soil')
+    elif len(next_ranges) == 6:
+        print('Processing soil-to-fertilizer')
+    elif len(next_ranges) == 5:
+        print('Processing fertilizer-to-water')
+    elif len(next_ranges) == 4:
+        print('Processing water-to-light')
+    elif len(next_ranges) == 3:
+        print('Processing light-to-temp')
+    elif len(next_ranges) == 2:
+        print('Processing temp-to-humidity')
+    elif len(next_ranges) == 1:
+        print('Processing humidity-to-location')
+    else:
+        print('Determining if lowest location')
+    if next_ranges == []:
+        if in_range[0] < lowest_location:
+            lowest_location = in_range[0]
+            print("found lowest location: " + str(lowest_location))
+        return
+    current_rule = next_ranges[0]
+    if len(next_ranges) > 1:
+        next_rule = next_ranges[1:]
+    else:
+        next_rule = []
+    ranges_to_check = [in_range]
+    success_translated_ranges = []
+    for rule in current_rule:
+        for range_to_check in ranges_to_check:
+            new_ranges_to_check = []
+            translated_ranges = split_ranges(range_to_check, rule[0], rule[1])
+            if translated_ranges[3] == True:
+                success_translated_ranges += [translated_ranges[1]]
+                if translated_ranges[0] != []:
+                    new_ranges_to_check += [translated_ranges[0]]
+                if translated_ranges[2] != []:
+                    new_ranges_to_check += [translated_ranges[2]]
+            else:
+                new_ranges_to_check += [range_to_check]
+        ranges_to_check = new_ranges_to_check[:]
+    for range_to_check in ranges_to_check:
+        success_translated_ranges += [range_to_check]
+    for next_range in success_translated_ranges:
+        translate_points(next_range, next_rule)  
+           
+
+
+for seed_range in seed_ranges:
+   translate_points(seed_range, rule_range)
+# translate_points(seed_ranges[1], rule_range)
+print()
+
 
 
 
